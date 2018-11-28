63f8593cd19458733cf7bbb0a001fc0c69e71b47
Fryguy1991 <fryman1991@gmail.com>
2018-11-28 14:35:19 -0600

    Attempt to modify AlbumsPager instead of creating my own.

--- spotify-api/src/main/java/kaaes/spotify/webapi/android/models/AlbumsFullPager.java
+++ spotify-api/src/main/java/kaaes/spotify/webapi/android/models/AlbumsFullPager.java
@@ -4,33 +4,33 @@ import android.os.Parcel;
 import android.os.Parcelable;
 
 public class AlbumsFullPager implements Parcelable {
-    public Pager<Album> albums;
-
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeParcelable(this.albums, 0);
-    }
-
-    public AlbumsFullPager() {
-    }
-
-    protected AlbumsFullPager(Parcel in) {
-        this.albums = in.readParcelable(Pager.class.getClassLoader());
-    }
-
-    public static final Parcelable.Creator<AlbumsFullPager> CREATOR = new Parcelable.Creator<AlbumsFullPager>() {
-        public AlbumsFullPager createFromParcel(Parcel source) {
-            return new AlbumsFullPager(source);
-        }
-
-        public AlbumsFullPager[] newArray(int size) {
-            return new AlbumsFullPager[size];
-        }
-    };
+//    public Pager<Album> albums;
+//
+//
+//    @Override
+//    public int describeContents() {
+//        return 0;
+//    }
+//
+//    @Override
+//    public void writeToParcel(Parcel dest, int flags) {
+//        dest.writeParcelable(this.albums, 0);
+//    }
+//
+//    public AlbumsFullPager() {
+//    }
+//
+//    protected AlbumsFullPager(Parcel in) {
+//        this.albums = in.readParcelable(Pager.class.getClassLoader());
+//    }
+//
+//    public static final Parcelable.Creator<AlbumsFullPager> CREATOR = new Parcelable.Creator<AlbumsFullPager>() {
+//        public AlbumsFullPager createFromParcel(Parcel source) {
+//            return new AlbumsFullPager(source);
+//        }
+//
+//        public AlbumsFullPager[] newArray(int size) {
+//            return new AlbumsFullPager[size];
+//        }
+//    };
 }
